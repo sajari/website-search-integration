621b10c9b6b606b5d8b67c7a6279509ed49d3e7a
Trent Billington <pudgepacket@gmail.com>
2017-08-10 17:32:38 +1000

    pass along placeholder to AutocompleteInput

--- src/InPage.js
+++ src/InPage.js
@@ -4,8 +4,14 @@ import { AutocompleteInput } from "sajari-react/ui/text";
 
 class InPage extends React.Component {
   render() {
-    const { pipeline, values } = this.props;
-    return <AutocompleteInput values={values} pipeline={pipeline} />;
+    const { pipeline, values, config } = this.props;
+    return (
+      <AutocompleteInput
+        values={values}
+        pipeline={pipeline}
+        placeholder={config.searchBoxPlaceHolder}
+      />
+    );
   }
 }
 
