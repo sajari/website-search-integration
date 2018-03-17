e0adc31be87bfddee4b69eb598008bc06d92b6c9
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-03-17 15:39:56 +1100

    package: move from cra to webpack@4
    * move to webpack build
    * move to preact via preact-compat
    * add bundle analyze tool

---
+++ config/webpack.dev.js
@@ -0,0 +1,24 @@
+const { resolve } = require("path");
+const merge = require("webpack-merge");
+const config = require("./webpack.config");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const argv = require("yargs").option("integration", {
+  describe: "integration type to run",
+  choices: ["inline", "searchbox", "overlay", "dynamic-content"],
+  default: "inline"
+}).argv;
+
+module.exports = merge(config, {
+  mode: "development",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, `../public/${argv.integration}.html`)
+    })
+  ],
+  serve: {
+    content: resolve(__dirname, "../dist")
+  }
+});
