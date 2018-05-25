4dd5e52f0b03ab6d067da3bb4beee0fe2731bd42
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-05-25 16:23:33 +1000

    package: update config, move to typescript

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   plugins: {
     autoprefixer: {
+<<<<<<< HEAD
       browsers: [">0.25%", "ie > 9", "not op_mini all"]
+=======
+      browsers: [">0.25%", "not ie 11", "not op_mini all"]
+>>>>>>> package: update config, move to typescript
     },
     cssnano: {}
   }
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,11 @@
 const { resolve } = require("path");
 
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 const VERSION = require("../package.json").version;
 
 module.exports = {
   target: "web",
   entry: {
-    "website-search": resolve(__dirname, "../src/index.js")
+    "website-search": resolve(__dirname, "../src/index.ts")
   },
   output: {
     path: resolve(__dirname, "../dist"),
@@ -16,38 +14,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: {
-            loader: "postcss-loader",
-            options: {
-              config: {
-                path: resolve(__dirname, "./postcss.config.js")
-              }
-            }
-          }
-        })
-      },
-      {
-        test: /\.js$/,
+        test: [/\.js$/, /\.tsx?/],
         exclude: [/node_modules/],
-        use: ["babel-loader"]
+        use: "babel-loader"
       }
     ]
   },
-  plugins: [new ExtractTextPlugin(`[name]-${VERSION}.css`)],
   resolve: {
-    extensions: [".js"],
-    alias: {
-      react: "preact-compat",
-      "react-dom": "preact-compat",
-      "create-react-class": "preact-compat/lib/create-react-class"
-
-      // So we load the ES6 module versions, without having to rewrite our import
-      // statements.
-      // sajari$: resolve(__dirname, "../node_modules/sajari/dist.es/main.js"),
-      // "sajari-react": resolve(__dirname, "../node_modules/sajari-react/es")
-    }
+    extensions: [".js", ".ts", ".tsx"]
   }
 };
