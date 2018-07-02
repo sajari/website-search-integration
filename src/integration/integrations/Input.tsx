2bf58b13a4b4f0ff8d739620f4e977528749fc2e
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-07-02 12:13:55 +1000

    integrations/Input: add autoFocus

--- src/integration/integrations/Input.tsx
+++ src/integration/integrations/Input.tsx
@@ -5,11 +5,12 @@ import { Input as SDKInput } from "@sajari/sdk-react";
 export interface InputProps {
   config: { [k: string]: any };
   autoComplete: boolean;
+  defaultValue?: string;
 }
 
 export class Input extends React.Component<InputProps> {
   public render() {
-    const { config, autoComplete } = this.props;
+    const { config, autoComplete, defaultValue } = this.props;
     const { mode, inputPlaceholder, inputAutoFocus } = config;
 
     const removeMarginBottom = mode === "search-box" || mode === "inline";
@@ -19,6 +20,8 @@ export class Input extends React.Component<InputProps> {
         inputMode={autoComplete ? "typeahead" : undefined}
         dropdownMode={autoComplete ? "suggestions" : undefined}
         placeholder={inputPlaceholder}
+        defaultValue={defaultValue}
+        autoFocus={inputAutoFocus}
         styles={
           removeMarginBottom ? { container: { marginBottom: 0 } } : undefined
         }
