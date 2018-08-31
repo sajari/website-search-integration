227a46d9a3009f9b0333b0cd3e4d3609f80afa95
Benjamin Hinchley <bhinchley@sajari.com>
2018-08-31 10:53:00 +1000

    WIP
    Change-Id: Ica7c39efc9a658bde59ed7b4c1a5c2b58c359a1c

---
+++ doczrc.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Sajari Website Search Integration",
+  description: "Easy to install website search integrations for Sajari",
+  source: "./docs",
+  themeConfig: {
+    colors: {
+      primary: "#459CE7"
+    },
+    styles: {
+      h1: {
+        fontSize: "4rem"
+      }
+    }
+  }
+};
